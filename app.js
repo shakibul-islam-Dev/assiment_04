506b59555ed827b732fc82c72db515dddf4cfcce
shakibul-isalm-dev <ishakibul704@gmail.com>
2026-02-22 03:25:58 +0600

    header Id dom add on variable

---
+++ app.js
@@ -0,0 +1,6 @@
+let grandToatal = document.getElementById('total')
+let interViewToatal = document.getElementById('interview')
+let rejecetdToatal = document.getElementById('rejecetd')
+let cardsToatal = document.getElementById('total-cards')
+let cards = document.querySelectorAll('.main-card')
+function setTotal() {}
